4f8f56489e5cb0bab6fe140a2f5939749d6fd7fc
baltasarromero <baltasarromero@gmail.com>
2023-02-27 18:50:59 -0300

    final changes

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,17 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import { useSigner, useAccount } from 'wagmi';
-import walletABI from '../abi/Wallet.json';
+import { useSigner } from 'wagmi';
 import wrapperABI from '../abi/ETHWrapper.json';
 import Button from '../components/ui/Button';
 
 const Wallet = () => {
   const { data: signer } = useSigner();
-  const { address } = useAccount();
 
-  const [contract, setContract] = useState();
-  const [userBalance, setUserBalance] = useState('0');
-  const [amount, setAmount] = useState('0');
   const [isLoading, setIsLoading] = useState(false);
   const [wrapperContract, setWrapperContract] = useState();
   const [signedMessage, setSignedMessage] = useState();
@@ -19,24 +14,10 @@ const Wallet = () => {
   const [tokenReceiver, setTokenReceiver] = useState();
   
 
-  const handleAmountChange = e => {
-    const { value } = e.target;
-    setAmount(value);
-  };
-
   useEffect(() => {
     if (signer) {
-       const _contract = new ethers.Contract(
-        '0x9D9955688649A7071a032DBf1e565023E6775690',
-        walletABI,
-        signer,
-      );
-
-     
-      setContract(_contract); 
-
       const _wrapperContract = new ethers.Contract(
-        '0x54713b609D83A5cb7162A453566f31D71aA7D994',
+        '0xcE6f72BbFa18B3c2eaDaF60C782c9699e40d44c0',
         wrapperABI.abi,
         signer,
       );
@@ -46,33 +27,6 @@ const Wallet = () => {
     }
   }, [signer]);
 
-  const getBalance = useCallback(async () => {
-    const result = await contract.userBalance(address);
-    const balanceFormatted = ethers.utils.formatEther(result);
-    setUserBalance(balanceFormatted);
-  }, [contract, address]);
-
-  useEffect(() => {
-    contract && getBalance();
-  }, [contract, getBalance]);
-
-  const handleDepositButtonClick = async () => {
-    setIsLoading(true);
-
-    try {
-      const tx = await contract.deposit({ value: ethers.utils.parseEther(amount) });
-      await tx.wait();
-
-      setAmount('0');
-
-      await getBalance();
-    } catch (e) {
-      console.log('e', e);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   const handleSingMessage = async () => {
     setIsLoading(true);
 
@@ -80,6 +34,7 @@ const Wallet = () => {
       const _messageHash = ethers.utils.solidityKeccak256(['string'], ["Yes, i Signed the message"]) 
       setHashedMessage(_messageHash);
       const _arrayfiedHash = ethers.utils.arrayify(_messageHash);
+      console.log("arrayified message ", _arrayfiedHash);
       const _signedMessage = await signer.signMessage(_arrayfiedHash);
       console.log("this is the signed message", _signedMessage);
       setSignedMessage(_signedMessage);
@@ -118,40 +73,12 @@ const Wallet = () => {
       }
   };
 
-  const handleWithdrawButtonClick = async () => {
-    setIsLoading(true);
-
-    try {
-      const tx = await contract.withdraw();
-      await tx.wait();
-
-      await getBalance();
-    } catch (e) {
-      console.log('e', e);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
     <div className="container my-5 my-lg-10">
       <div className="row">
         <div className="col-6">
-          <h1 className="heading-medium mb-5">Basic contract interaction</h1>
+          <h1 className="heading-medium mb-5">Wrap using signed message</h1>
           <div className="d-flex align-items-center">
-            <div>
-              <input
-                type="text"
-                className="form-control"
-                value={amount}
-                onChange={handleAmountChange}
-              />
-            </div>
-            <div className="ms-3">
-              <Button loading={isLoading} onClick={handleDepositButtonClick} type="primary">
-                Deposit
-              </Button>
-            </div>
             <div className="ms-3">
               <Button loading={isLoading} onClick={handleSingMessage} type="primary">
                 Sign Message
@@ -165,14 +92,7 @@ const Wallet = () => {
           </div>
         </div>
       </div>
-      <div className="mt-5">{userBalance} ETH</div>
-      {Number(userBalance) > 0 ? (
-        <div className="mt-2">
-          <Button loading={isLoading} onClick={handleWithdrawButtonClick} type="primary">
-            Withdraw
-          </Button>
-        </div>
-      ) : null}
+
     </div>
     
   );
